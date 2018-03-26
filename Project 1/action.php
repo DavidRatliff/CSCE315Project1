7c476d8287c0a556bd5114cb107fdd77707db78a
isaacoviedo <isaac2850@gmail.com>
2018-03-26 11:25:03 -0500

    Finished implementing most functions in "action.php". Changed interface to no longer offer median and mode functions.

--- Project 1/action.php
+++ Project 1/action.php
@@ -12,45 +12,46 @@
 	$endTime = $_GET["endTime"];
 
 		
-	if($_GET["highOfWeek"]) {
+	if($_GET["highOfWeek"]) 
+	{
 		getHigh($startDate,$endDate,$startTime,$endTime);
 	}
-	else if($_GET["lowOfWeek"]) {
-		echo("Chose low of the week");
+	else if($_GET["lowOfWeek"]) 
+	{
+		getLow($startDate,$endDate,$startTime,$endTime);
 	}
-	else if($_GET["avgTF"]) {
+	else if($_GET["avgTF"]) 
+	{
 		getAvg($startDate,$endDate,$startTime,$endTime);
 	}
-	else if($_GET["medTF"]) {
-		echo("Chose median");
-	}
-	else if($_GET["modeTF"]) {
-	
-	}
-	else if($_GET["visualRep"]) {
+	else if($_GET["visualRep"]) 
+	{
 		
 	}
-	else if($_GET["numInTF"]) {
+	else if($_GET["numInTF"]) 
+	{
 		getNumUserBetween($startDate,$endDate,$startTime,$endTime);
 	}
-	else {
+	else 
+	{
 		echo("Error: Unknown Option Selected");
 	}
 	
 	
 	//Helper function used to convert string arguments to SQL date format
-	function formatDate($date,$time) {
+	function formatDate($date,$time) 
+	{
 		$timestamp = strtotime($date . " " . $time);
 		$datetime = date("Y-m-d H:i", $timestamp);
-		//echo($datetime);
 		
 		return $datetime;
 	}
 	
 	
 	
-	//Finished
-	function getNumUserBetween($startD,$endD,$startT,$endT) {
+	// Finished
+	function getNumUserBetween($startD,$endD,$startT,$endT) 
+	{
 		global $debug; global $COMMON;
 		
 		$beginning = formatDate($startD,$startT);
@@ -60,40 +61,57 @@
 		$rs = $COMMON->executeQuery($sql, $_SERVER["SCRIPT_NAME"] );
 		$row = $rs->fetch(PDO::FETCH_NUM);
 
-		echo("<h3>Number of students between " . $beginning . " and " . $end . ": " . $row[0] ."</h3>");
+		echo("<h3> Number of students between " . $startD . " at " . $startT . " and " . $endD . " at " . $endT . ": " . $row[0] . "</h3>");
+
+	}
+	
+	// Finished
+	function getLow($startD, $endD, $startT, $endT) 
+	{
+		global $debug; global $COMMON;
+		
+		$sql = "SELECT MIN(COUNT)FROM ( SELECT COUNT(*) AS COUNT FROM `motion_data` WHERE (CAST(visit_time AS DATE) BETWEEN '$startD' AND '$endD') AND (CAST(visit_time AS TIME) BETWEEN '$startT' AND '$endT') GROUP BY CAST(visit_time AS DATE) ) AS COUNTS";
+		$rs = $COMMON->executeQuery($sql, $_SERVER["SCRIPT_NAME"]);
+		$row = $rs->fetch(PDO::FETCH_NUM);
+		
+		echo("<h3> The low point of the time frame was: " . $row[0] . " visit(s). </h3>");
+		
 	}
 	
 	
-	//Needs implementation
-	function getHigh($startD,$endD,$startT,$endT) {
+	// Finished
+	function getHigh($startD,$endD,$startT,$endT) 
+	{
 		global $debug; global $COMMON;
 
-		$beginning = formatDate($startD,$startT);
-		$end = formatDate($endD, $endT);
-		
+		$sql = "SELECT MAX(COUNT)FROM ( SELECT COUNT(*) AS COUNT FROM `motion_data` WHERE (CAST(visit_time AS DATE) BETWEEN '$startD' AND '$endD') AND (CAST(visit_time AS TIME) BETWEEN '$startT' AND '$endT') GROUP BY CAST(visit_time AS DATE) ) AS COUNTS";
+		$rs = $COMMON->executeQuery($sql, $_SERVER["SCRIPT_NAME"] );
+		$row = $rs->fetch(PDO::FETCH_NUM);
+
+		echo("<h3> The high point of the time frame was: " . $row[0] . " visit(s). </h3>");
 		
 	}
 	
 	
-	//Needs implementation
-	function getAvg($startD,$endD,$startT,$endT) {
+	// Finished
+	function getAvg($startD,$endD,$startT,$endT) 
+	{
 		global $debug; global $COMMON;
 		
 		$beginning = formatDate($startD,$startT);
 		$end = formatDate($endD, $endT);
-		
-		$sql = "SELECT COUNT(*) FROM `motion_data` WHERE `visit_time` BETWEEN '$beginning' AND '$end'";
+
+		$sql = "SELECT AVG(COUNT)FROM ( SELECT COUNT(*) AS COUNT FROM `motion_data` WHERE (CAST(visit_time AS DATE) BETWEEN '$startD' AND '$endD') AND (CAST(visit_time AS TIME) BETWEEN '$startT' AND '$endT') GROUP BY CAST(visit_time AS DATE) ) AS COUNTS";
 		$rs = $COMMON->executeQuery($sql, $_SERVER["SCRIPT_NAME"] );
 		$row = $rs->fetch(PDO::FETCH_NUM);
-	
-		
-	
-		echo($row[0]);
+
+		echo("<h3> Average number of students between " . $startD . "-" . $endD . " during " . $startT . "-" . $endT . ": " . $row[0] . "</h3>");
 	}
 	
 	
 	//Needs implementation. Suggested on piazza to look into google charts.
-	function visualRepresentation() {
+	function visualRepresentation() 
+	{
 
 	}
 	
--- Project 1/interface.php
+++ Project 1/interface.php
@@ -2,13 +2,9 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-
-<script>
-</script>
-
 <body>
 
-	<h3> Choose the start/end date and start/end time. If you wish you may leave the end time empty. </h3>
+	<h3> Choose the start/end date and start/end time. </h3>
 	
     <form action = "action.php" method = "get">
 		Select Start Date: <input type = "date" name = "startDate">
@@ -17,18 +13,14 @@
 		Select End Time: <input type = "time" name = "endTime"> <br> <br>
 
 		Graph data points for visual representation: <input type = "submit" value = "Option 1" name = "visualRep"> <br>
-		High points of the week: <input type = "submit" value = "Option 2" name = "highOfWeek"> <br>
-		Low points of the week: <input type = "submit" value = "Option 3" name = "lowOfWeek"> <br>
-		Mean of given time frame: <input type = "submit" value = "Option 4" name = "avgTF"> <br>
-		Median of given time frame: <input type = "submit" value = "Option 5" name = "medTF"> <br>
-		Mode of given time frame: <input type = "submit" value = "Option 6" name = "modeTF"> <br>
-		Number of students in certain time frame: <input type = "submit" value = "Option 7" name = "numInTF"> <br>
+		High points of given time frame: <input type = "submit" value = "Option 2" name = "highOfWeek"> <br>
+		Low points of given time frame: <input type = "submit" value = "Option 3" name = "lowOfWeek"> <br>
+		Average number of visits of given time frame: <input type = "submit" value = "Option 4" name = "avgTF"> <br>
+		<!--Median of given time frame: <input type = "submit" value = "Option 5" name = "medTF"> <br>
+		Mode of given time frame: <input type = "submit" value = "Option 6" name = "modeTF"> <br> -->
+		Total number of visits of given time frame: <input type = "submit" value = "Option 5" name = "numInTF"> <br>
 	</form>
 
-	<!--<form>
-		<input type = "submit" name = "subButton">
-	</form>
-	-->
 	
 </body>
 </head>
