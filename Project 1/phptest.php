ef8e40f21ae92215ae41d542343b04ef2f3c5f0a
isaacoviedo <isaac2850@gmail.com>
2018-03-27 23:57:51 -0500

    Updated test cases in phptest.php

--- Project 1/phptest.php
+++ Project 1/phptest.php
@@ -2,50 +2,139 @@
 	
 // Function Signatures:
 
-	// Returns the number of visitors between time begin and end
-	// Uses the present time as default end value
-	// Preconditions: Successful connection to SQL database, parameters are timestamps in string form, $begin>epoch, $end<current time, $begin<$end
-	// Postconditions: Returns visit count
-	// function visitors_between($begin, $end="CURRENT_TIMESTAMP")
+	// Returns the number of visitors in the time frame given
+	// Uses start date, end date, start time, and end time given by user
+	// Preconditions: Successful connection to SQL database, parameters are timestamps in string form, $startD>epoch, $end<current time, $startT and $endT are valid times
+	// Postconditions: Display number of visitors in time frame
+	// function echoVisitsBetween($startD, $endD, $startT, $endT)
 
-
-	// Returns list of visit times between time begin and end
-	// Uses present time as default end value
-	// Preconditions: Successful connection to SQL database, parameters are timestamps in string form, $begin>epoch, $end<current time
-	// Postconditions: Returns list of visit times
-	// function visit_times_between($begin, $end="CURRENT_TIMESTAMP")
-
-
+	
+	// Returns the low point in the time frame given
+	// Uses start date, end date, start time, and end time given by user
+	// Preconditions: Successful connection to SQL database, parameters are timestamps in string form, $startD>epoch, $end<current time, $startT and $endT are valid times
+	// Postconditions: Displays least number of visits and corresponding date
+	// function echoLow($startD, $endD, $startT, $endT)
+	
+	
+	// Returns the high point in the time frame given
+	// Uses start date, end date, start time, and end time given by user
+	// Preconditions: Successful connection to SQL database, parameters are timestamps in string form, $startD>epoch, $end<current time, $startT and $endT are valid times
+	// Postconditions: Displays greatest number of visits and corresponding date
+	// function echoHigh($startD, $endD, $startT, $endT)
+	
+	
+	// Returns the average number of visits in the time frame given
+	// Uses start date, end date, start time, and end time given by user
+	// Preconditions: Successful connection to SQL database, parameters are timestamps in string form, $startD>epoch, $end<current time, $startT and $endT are valid times
+	// Postconditions: Displays average number of visits
+	// function echoAvg($startD, $endD, $startT, $endT)
+	
 
 use PHPUnit\Framework\TestCase;
 
 final class ProjectTest extends TestCase {
 
-	// Case 1: Invalid parameter types
+	// Cases 1-4: Invalid parameter types
 	public function test1(): void {
 		$this->expectException(InvalidArgumentException::class);
 
-		visitors_between(1, TRUE)
+		echoVisitsBetween(1, TRUE, 2, FALSE);
 	}
 
-	// Case 2: Future date
 	public function test2(): void {
 		$this->expectException(InvalidArgumentException::class);
-
-		visitors_between("2017-02-05 13:27:07", "2021-02-05 13:27:07")
+		
+		echoHigh(1, TRUE, 2, FALSE);
 	}
-
-	// Case 3: Date before epoch
+	
+	
 	public function test3(): void {
 		$this->expectException(InvalidArgumentException::class);
-
-		visitors_between("1917-02-05 13:27:07", "2016-02-05 13:27:07")
+		
+		echoLow(1, TRUE, 2, FALSE);
 	}
-
-	// Case 4: Begin date after end date
+	
 	public function test4(): void {
 		$this->expectException(InvalidArgumentException::class);
-
-		visitors_between("2017-02-05 13:27:07", "2015-02-05 13:27:07")
+		
+		echoAvg(1, TRUE, 2, FALSE);
 	}
+	
+
+	// Case 5-8: Future date
+	public function test5(): void {
+		$this->expectException(InvalidArgumentException::class);
+
+		echoVisitsBetween("2017-02-05", "2021-02-05", "13:27:07", "13:27:07");
+	}
+
+	public function test6(): void {
+		$this->expectException(InvalidArgumentException::class);
+
+		echoHigh("2017-02-05", "2021-02-05", "13:27:07", "13:27:07");
+	}
+	
+	public function test7(): void {
+		$this->expectException(InvalidArgumentException::class);
+
+		echoLow("2017-02-05", "2021-02-05", "13:27:07", "13:27:07");
+	}
+	
+	public function test8(): void {
+		$this->expectException(InvalidArgumentException::class);
+
+		echoAvg("2017-02-05", "2021-02-05", "13:27:07", "13:27:07");
+	}
+
+
+	// Case 9-12: Date before epoch
+	public function test9(): void {
+		$this->expectException(InvalidArgumentException::class);
+
+		echoVisitsBetween("1917-02-05", "2016-02-05", "13:27:07", "13:27:07");
+	}
+	
+	public function test10(): void {
+		$this->expectException(InvalidArgumentException::class);
+
+		echoHigh("1917-02-05", "2016-02-05", "13:27:07", "13:27:07");
+	}	
+	
+	public function test11(): void {
+		$this->expectException(InvalidArgumentException::class);
+
+		echoLow("1917-02-05", "2016-02-05", "13:27:07", "13:27:07");
+	}	
+	
+	public function test12(): void {
+		$this->expectException(InvalidArgumentException::class);
+
+		echoAvg("1917-02-05", "2016-02-05", "13:27:07", "13:27:07");
+	}	
+
+	// Case 13-16: Begin date after end date
+	public function test13(): void {
+		$this->expectException(InvalidArgumentException::class);
+
+		echoVisitsBetween("2017-02-05", "2015-02-05", "13:27:07", "13:27:07");
+	}
+	
+	public function test14(): void {
+		$this->expectException(InvalidArgumentException::class);
+
+		echoHigh("2017-02-05", "2015-02-05", "13:27:07", "13:27:07");
+	}
+	
+	public function test15(): void {
+		$this->expectException(InvalidArgumentException::class);
+
+		echoLow("2017-02-05", "2015-02-05", "13:27:07", "13:27:07");
+	}
+	
+	public function test16(): void {
+		$this->expectException(InvalidArgumentException::class);
+
+		echoAvg("2017-02-05", "2015-02-05", "13:27:07", "13:27:07");
+	}
+	
 }
