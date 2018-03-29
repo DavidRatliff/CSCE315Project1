d4f4c7105cabb2c96ed40107dddda2331e122519
DavidRatliff <33940325+DavidRatliff@users.noreply.github.com>
2018-03-29 11:22:38 -0500

    removed second pir sensor from arduino.py

--- arduino.py
+++ arduino.py
@@ -2,30 +2,26 @@ import pyfirmata
 import time
 import threading
 import sys
-from edgedetect import EdgeDetector
 
 board = None
 board_port = "COM1"
-pir1_pin = None
-pir2_pin = None
+pir_pin = None
 
 # Sets up Arduino
 # Preconditions: Arduino is connected to PC port COM1
-# Postconditions: Board is initialized, pir1_pin, pir2_pin contain references to pir sensors on digital ports 12, 13
+# Postconditions: Board is initialized, pir_pin contains reference to PIR sensor on pin 13
 def arduino_setup():
 	board = pyfirmata.Arduino(board_port)
 
 	it = pyfirmata.util.Iterator(board)
 	it.start()
 
-	pir1_pin = board.get_pin('d:12:i')
-	pir1_pin.enable_reporting()
-	pir2_pin = board.get_pin('d:13:i')
-	pir2_pin.enable_reporting()
+	pir_pin = board.get_pin('d:13:i')
+	pir_pin.enable_reporting()
 
 # Monitors PIR sensors for one-way motion
-# Calls motion_detected() if pir1 is activated, followed by pir2, indicating someone entering building
-# Preconditions: setup() has completed successfully, pir1_pin and pir2_pin connected to PIR sensors
+# Calls motion_detected() if pir detects motion
+# Preconditions: setup() has completed successfully, pir_pin connected to PIR sensor
 # Postconditions: Function does not exit. Calls motion_detected() on HIGH input from PIR.
 def arduino_loop():
 	while pir1_pin.read() is None:
@@ -33,8 +29,7 @@ def arduino_loop():
 
 	while True:
 		if pir1_pin.read() is True:
-			EdgeDetector pir2_ed(pir2_pin.read(), motion_detected())
-			time.sleep(4)
+			motion_detected()
 
 
 # Callback for detecting motion with PIR
